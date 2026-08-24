440a351ba2558214cb355731cfeaac51bb49ece3
ell <ell.mhrni31@gmail.com>
2026-08-24 17:53:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const songs = [
   [ "'Cause You Have To", "LANY", 251, "'Cause You Have To.mp3"],
   [ "Merry Christmas, Please Don't Call", "Bleachers", 203, "Merry Christmas, Please Don't Call.mp3"],
   [ "Past Life", "Ariana Grande", 216, "Ariana Grande - Past Life.mp3"],
+  [ "Location Unknow", "HONNE feat BEKA", 324, "Location Unknow.mp3"],
 ];
 
 const $ = (id) => document.querySelector(id);
